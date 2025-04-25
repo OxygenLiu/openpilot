0531b11a48bfe1a4cc9a392732709a5ebb3a11e5
oxygen <oxygen.liu@gmail.com>
2025-04-26 05:44:08 +0800

    wheel re-alignment and torque params update

--- selfdrive/car/bmw/interface.py
+++ selfdrive/car/bmw/interface.py
@@ -109,7 +109,7 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.torque.kp = 1.0 / CarControllerParams.STEER_MAX
     ret.lateralTuning.torque.ki = 0.5 / CarControllerParams.STEER_MAX
     ret.lateralTuning.torque.kf = 8.5 / CarControllerParams.STEER_MAX
-    ret.lateralTuning.torque.latAccelOffset = -0.190
+    ret.lateralTuning.torque.latAccelOffset = -0.253
     ret.lateralTuning.torque.useSteeringAngle = True
     ret.lateralTuning.torque.steeringAngleDeadzoneDeg = 0.0 # backlash of stepper?
 
